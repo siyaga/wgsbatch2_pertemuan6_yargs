1625916d7b2b42597bae189dbe6835243715625d
lifeforme <siyaga123@gmail.com>
2022-07-11 09:29:42 +0700

    Membuat inputan menggunakan yargs dan menyimpan array tersebut di dalam array contact

--- app.js
+++ app.js
@@ -1,6 +1,59 @@
-// const validator = require('validator');
-// const fs = require('fs');
-// const readline = require('readline');
+0
+// Memanggil Yargs
+const yargs = require("yargs");
+
+// Membuat Yargs 
+yargs.command({
+    command:'add',
+    describe:'add new contact',
+    builder:{
+        //membuat isi array deksripsinya
+        // Membuat nama
+        name: {
+            describe:'Contact name',
+            demandOption:true,
+            type:'string',
+        },
+        // membuat email
+        email:{
+            describe:'Contacct Email',
+            demandOption:false,
+            type:'string',
+        },
+        // membuat Mobile
+        mobile:{
+            describe:'Contact Mobile phone number',
+            demandOption:true,
+            type:'string',
+        },
+
+    },
+    //Memanggil yargs
+    handler(argv){
+        //membuat array contact yang akan di isi oleh isi yargs tadi di atas
+        const contact = {
+            name: argv.name,
+            email: argv.email,
+            mobile: argv.mobile,
+        };
+        //Menampilkan array yang sudah di inputkan
+        console.log(contact);
+    },
+});
+// Menjalankan yargs utama
+yargs.parse();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const { questions,saveIsiData } = require('./contact') ;
